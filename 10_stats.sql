224ed5dbf8543d826b75ff1c97e028672c179d18
GabrielVitorf6 <gabrielvitorf6@gmail.com>
2026-07-26 19:57:40 -0300

    minnor changes

--- 10_stats.sql
+++ 10_stats.sql
@@ -1,31 +1,31 @@
 -- SOMA DE PONTOS MOVIMENTADOS DO MÊS DE JULHO, QUANTIDADE DE PONTOS GANHOS E QUANTIDADE DE PONTOS GASTOS
 
--- SELECT  
---         SUM(qtdePontos) AS SaldoTotal,
+SELECT  
+        SUM(qtdePontos) AS SaldoTotal,
 
---         SUM(
---         CASE
---             WHEN qtdePontos > 0 THEN qtdePontos
---             END) AS qtdePontosPositivos,
+        SUM(
+        CASE
+            WHEN qtdePontos > 0 THEN qtdePontos
+            END) AS qtdePontosPositivos,
 
---         SUM(
---         CASE
---             WHEN qtdePontos < 0 THEN qtdePontos
---             END) AS qtdePontosNegativos,
+        SUM(
+        CASE
+            WHEN qtdePontos < 0 THEN qtdePontos
+            END) AS qtdePontosNegativos,
 
---         COUNT(
---             CASE
---             WHEN qtdePontos < 0 THEN qtdePontos
---             END) AS qtdeTransacoesNegativas,
+        COUNT(
+            CASE
+            WHEN qtdePontos < 0 THEN qtdePontos
+            END) AS qtdeTransacoesNegativas,
 
---         COUNT(
---             CASE
---             WHEN qtdePontos > 0 THEN qtdePontos
---             END) AS qtdeTransacoesPositivas
+        COUNT(
+            CASE
+            WHEN qtdePontos > 0 THEN qtdePontos
+            END) AS qtdeTransacoesPositivas
 
--- from transacoes
+from transacoes
 
--- WHERE DtCriacao >= '2025-07-01' AND DtCriacao < '2025-08-01'
+WHERE DtCriacao >= '2025-07-01' AND DtCriacao < '2025-08-01'
 
 
 -- ESTATÍSTICAS DO SALDO DE CONTA DOS CLIENTES
