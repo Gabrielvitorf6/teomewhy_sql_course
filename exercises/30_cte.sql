6767fac42dca6c614574dbfeba1c9c6ce574eac4
GabrielVitorf6 <gabrielvitorf6@gmail.com>
2026-09-13 21:19:51 -0300

    Aula 24: Exercícios de CTEs

--- exercises/30_cte.sql
+++ exercises/30_cte.sql
@@ -17,18 +17,31 @@
 
 
 WITH tb_clientes_d1 AS (
+
         SELECT DISTINCT idCliente
+
         FROM transacoes
+
         WHERE substr(DtCriacao, 1, 10) = '2025-08-25'
 ),
 
-tb_clientes_fulltime AS (
-
-SELECT idCliente,
-        DtCriacao
-        FROM transacoes
-        WHERE dtCriacao >= '2025-08-25' AND dtCriacao < '2025-08-30'
-
-)
-
+tb_join AS (
 SELECT 
+
+        substr(t2.DtCriacao, 1, 10) AS dtDia,
+        COUNT(DISTINCT t1.idCliente) AS qtdeClientes,
+        1. * COUNT(DISTINCT t1.idCliente) / (SELECT COUNT(*) FROM tb_clientes_d1) AS pctRetencao,  
+        1 - 1. * COUNT(DISTINCT t1.idCliente) / (SELECT COUNT(*) FROM tb_clientes_d1) AS pctchurn
+
+FROM tb_clientes_d1 AS t1
+
+LEFT JOIN transacoes AS t2
+USING (idCliente)
+
+WHERE t2.DtCriacao >= '2025-08-25'
+        AND t2.DtCriacao < '2025-08-30'
+
+GROUP BY 1)
+
+SELECT *
+FROM tb_join;
